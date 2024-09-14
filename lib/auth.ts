d405ce9e6cacba189b2dbf8d15f4e662bee6f127
ArekBartnik <arek.bartnik@gmail.com>
2024-09-14 22:09:29 +0200

    fixes auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth, { type Account, type Session, type User } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 import Spotify from "next-auth/providers/spotify";
+import { env } from "./env";
 
 declare module "next-auth" {
 	interface Session {
@@ -19,7 +20,15 @@ export interface AuthUser extends User {
 }
 
 const authURL = `https://accounts.spotify.com/authorize?${new URLSearchParams({
-	scope: "user-read-email user-follow-modify user-follow-read user-library-read user-library-modify",
+	scope: [
+		"user-read-email",
+		"user-follow-modify",
+		"user-follow-read",
+		"user-library-read",
+		"user-library-modify",
+		"user-read-playback-state",
+		"user-modify-playback-state",
+	].join(" "),
 }).toString()}`;
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -30,12 +39,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 		}),
 	],
 	session: {
-		maxAge: 60 * 60, // 1hr
+		maxAge: 60 * 60 * 24, // 1day
 	},
 	debug: process.env.NODE_ENV === "development",
 	callbacks: {
 		async jwt({ token, account }: { token: JWT; account: Account | null }) {
+			const now = Math.floor(Date.now() / 1000);
+
 			if (!account) {
+				if (now > (token.expiresAt as number)) {
+					return await refreshAccessToken(token);
+				}
 				return token;
 			}
 
@@ -43,15 +57,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 				...token,
 				accessToken: account.access_token,
 				tokenType: account.token_type,
-				expiresAt: account.expires_at ?? Date.now() / 1000,
-				expiresIn: (account.expires_at ?? 0) - Date.now() / 1000,
+				expiresAt: account.expires_at as number,
+				expiresIn: account.expires_in as number,
 				refreshToken: account.refresh_token,
 				scope: account.scope,
 				id: account.providerAccountId,
 			};
 
-			if (Date.now() < updatedToken.expiresAt) {
-				return refreshAccessToken(updatedToken);
+			if (now > updatedToken.expiresAt) {
+				return await refreshAccessToken(updatedToken);
 			}
 
 			return updatedToken;
@@ -75,12 +89,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 });
 
 async function refreshAccessToken(token: JWT): Promise<JWT> {
+	console.log("Refreshing access token");
 	try {
-		const response = await fetch(authURL, {
+		const response = await fetch("https://accounts.spotify.com/api/token", {
 			headers: {
 				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization:
+					"Basic " +
+					Buffer.from(
+						env.AUTH_SPOTIFY_ID + ":" + env.AUTH_SPOTIFY_SECRET,
+					).toString("base64"),
 			},
 			method: "POST",
+			body: new URLSearchParams({
+				grant_type: "refresh_token",
+				refresh_token: token.refreshToken as string,
+			}),
 		});
 
 		const refreshedTokens = await response.json();
@@ -89,17 +113,18 @@ async function refreshAccessToken(token: JWT): Promise<JWT> {
 			throw refreshedTokens;
 		}
 
+		const now = Math.floor(Date.now() / 1000);
+
 		return {
 			...token,
 			accessToken: refreshedTokens.access_token,
-			tokenType: refreshedTokens.token_type,
-			expiresAt: refreshedTokens.expires_at,
-			expiresIn: (refreshedTokens.expires_at ?? 0) - Date.now() / 1000,
+			expiresAt: now + refreshedTokens.expires_in,
+			expiresIn: refreshedTokens.expires_in,
 			refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
 			scope: refreshedTokens.scope,
 		};
 	} catch (error) {
-		console.error(error);
+		console.error("Error refreshing access token", error);
 		return {
 			...token,
 			error: "RefreshAccessTokenError",
